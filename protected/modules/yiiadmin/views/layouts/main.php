96f117d71971d48593248f8711922141103c3828
Owen chen <chenxin448@126.com>
2012-08-06 00:26:21 +0800

    admin en->zh

--- protected/modules/yiiadmin/views/layouts/main.php
+++ protected/modules/yiiadmin/views/layouts/main.php
@@ -6,7 +6,7 @@ $cs->registerCssFile($baseUrl.'/css/base.css');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" lang="" xml:lang="" > 
 <head> 
-    <title>Yii Administration - <?php echo $this->pageTitle; ?></title> 
+    <title>鼎峰国际 - <?php echo $this->pageTitle; ?></title> 
     <meta name="robots" content="NONE,NOARCHIVE" /> 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
 </head> 
@@ -15,7 +15,7 @@ $cs->registerCssFile($baseUrl.'/css/base.css');
 
 <div id="header"> 
     <div class="branding">&nbsp;</div> 
-    <div class="admin-title">Yii Administration</div> 
+    <div class="admin-title">鼎峰国际管理</div> 
 	<ul class="nav">
 		<li><a <?php if(isset($_GET['model_name']) && $_GET['model_name'] == "About"):?>class="active" <?php endif;?> href="<?php echo $this->createUrl('manageModel/list/model_name/About');?>">公司简介</a></li>
 		<li><a <?php if(isset($_GET['model_name']) && $_GET['model_name'] == "Hairstylist"):?>class="active" <?php endif;?> href="<?php echo $this->createUrl('manageModel/list/model_name/Hairstylist');?>">发型师</a></li>
